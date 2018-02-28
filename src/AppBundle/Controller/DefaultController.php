a2867e19fbfcc6585aca6f4db331823d243a7ce7
Fatimah Gons saib <gonssaib@hotmail.fr>
2018-02-28 10:08:12 +0100

    Adding template to symfony

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,9 +2,15 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Film;
+use AppBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class DefaultController extends Controller
 {
@@ -26,6 +32,8 @@ class DefaultController extends Controller
     {
         // replace this example code with whatever you need
         return $this->render('default/accueil.html.twig');
+
+
     }
 
 
@@ -44,15 +52,43 @@ class DefaultController extends Controller
      */
     public function addInscriptionAction()
     {
-        $project = new Project();
-        $form = $this->createFormBuilder($project)
-            ->add('name', TextType:: class)
-            ->add('description', TextareaType:: class)
-            ->add('completion', NumberType:: class)
-            ->add('save', SubmitType:: class, ['label' => 'Ajouter un projet'])
+        $user = new User();
+        $form = $this->createFormBuilder($user)
+            ->add('firstname', TextType:: class)
+            ->add('lastname', TextType:: class)
+            ->add('mail', EmailType:: class)
+            ->add('createdAt', DateType:: class)
+            ->add('password', TextType:: class)
+            ->add('save', SubmitType:: class, ['label' => 'S inscrire'])
             ->getForm();
-        return $this->render('project/inscription.html.twig', [
+        return $this->render('default/inscription.html.twig', [
             'form' => $form->createView()
         ]);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $project = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+            return $this->redirectToRoute('netflix');   }
+
+
+
+    }
+
+    /**
+     * @Route("/films", name="films")
+     */
+    public function filmsAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        //return $this->render('default/films.html.twig');
+
+        $em = $this->getDoctrine()->getManager();
+        $films = $em->getRepository(Film:: class)
+            ->findAll();
+        return $this->render('default/films.html.twig', [
+            'films' => $films        ]);
     }
 }
