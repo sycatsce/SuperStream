08130e5df726b716bd10d8d417c45ef3aa9092b4
Sycatsce <brun.emmanuel@hotmail.com>
2018-02-28 14:37:27 +0100

    inscription&form

---
+++ src/AppBundle/Controller/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Film;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class IndexController extends Controller
+{
+    /**
+     * @Route("/", name="/")
+     */
+    public function indexAction(Request $request)
+    {
+        return $this->render('default/accueil.html.twig');
+    }
+}
